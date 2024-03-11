3aef39e0190d463acc369f46d868fbee2c185186
s28922 <s28922@pjwstk.edu.pl>
2024-03-11 18:12:07 +0100

    Modyfikacja1

---
+++ zad1/zad1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program {
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        int[] tab1 = { 1, 2, 3, 4, 5, 6 };
+    }
+}
