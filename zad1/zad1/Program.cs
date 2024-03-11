9fdd7adaaba4cfebc014bf4da33dc4bb264965b1
s28922 <s28922@pjwstk.edu.pl>
2024-03-11 18:17:00 +0100

    Modyfikacja2

--- zad1/zad1/Program.cs
+++ zad1/zad1/Program.cs
@@ -1,9 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Threading.Tasks.Dataflow;
+
 class Program {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+       
         int[] tab1 = { 1, 2, 3, 4, 5, 6 };
+        int size = tab1.Length;
+        foreach (int num in tab1)
+        {
+            Console.WriteLine(num); 
+        }
     }
 }
